d3d3f863cf79b2dffc39a7f45ba1c4e42996c0e5
Okens <Ng.Eric314@gmail.com>
2018-06-03 19:37:49 -0700

    Linking and adding styling css sheet.

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ let PORT = process.env.PORT || 8080;
 //Express to handle data parsing.
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-// app.use("./app", express.static(__dirname + "./app"));
+app.use("/app/css", express.static(__dirname + "/app/css"));
+// app.use("/app/data", express.static(__dirname + "/app/data"));
 
 //JS files listening for requests in apiRoutes.js and htmlRoutes.js
 // require("./app/data/questions")(app);
