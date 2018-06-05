9fae56ba5ff9d0b19187de58ae7eb6aa2d521184
Okens <Ng.Eric314@gmail.com>
2018-06-04 20:36:36 -0700

    Adding a link for server side to reset array back to default, will not provide in html page.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,10 +12,10 @@ module.exports = (app) => {
         // Get corresponding person's objectData        
         for (let i = 0; i < trainersData.length; i++) {
             let checkCompat = 0;
-            for(let j = 0; j < trainersData[i]["trainerPref"].length; j++){
+            for (let j = 0; j < trainersData[i]["trainerPref"].length; j++) {
                 checkCompat += Math.abs(req["body"]["trainerPref"][j] - trainersData[i]["trainerPref"][j]);
             }
-            if(checkCompat < compatibility){
+            if (checkCompat < compatibility) {
                 pairedWithObj = trainersData[i];
                 compatibility = checkCompat;
             }
@@ -26,5 +26,32 @@ module.exports = (app) => {
 
         trainersData.push(req.body);
         res.json(pairedWithObj);
+    })
+
+    //link to reset the array to default, not provided in html.
+    app.get('/resetInfoToDefault', (req, res) => {
+        console.log("entered");
+        let defaultArr = [
+            {
+                trainerName: "Ash Ketchum",
+                trainerEmail:"AshKetchum@PalletTown.com",
+                trainerPhone:"415-555-8888",
+                trainerImage: "http://cdn.playbuzz.com/cdn/7649eea9-e2e8-4001-906e-d647321a5533/ad720a79-5767-477f-85ea-f398b414d509_560_420.jpg",
+                trainerPref: [5,5,5,5,5,5,5,5,5,5]
+            },
+            {
+                trainerName: "Tom Ato",
+                trainerEmail:"TomAto@PalletTown.com",
+                trainerPhone:"415-555-8888",
+                trainerImage: "https://i.ytimg.com/vi/oKBXSKMXUCY/maxresdefault.jpg",
+                trainerPref: [1,1,1,1,1,1,1,1,1,1]
+            }
+        ];
+
+        trainersData.length = 0;
+        trainersData.push(defaultArr[0]);
+        trainersData.push(defaultArr[1]);
+
+    res.json(trainersData);
     })
 };
